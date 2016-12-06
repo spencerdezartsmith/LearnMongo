468091e319d452ca1b85996c2928a932e9792424
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-06 14:50:22 -0800

    Add the before wrapper around the mongoose connection. Add beforeEach function to drop the db before test

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,15 +1,25 @@
 // All code needed to set up our testing environment
 const mongoose = require('mongoose');
+// Referencing the ES6 implementation of promises
+mongoose.Promise = global.Promise;
 
-// Connect to the database. It will create if the db doesn't exist
-mongoose.connect('mongodb://localhost/users_test');
-// Event handlers for the connection
-mongoose.connection
-  .once('open', () => {
-    console.log('Good to go!')
-  })
-  .on('error', (error) => {
-    console.warn('Warning', error)
+// We only want to connect to mongo once
+before((done) => {
+  // Connect to the database. It will create if the db doesn't exist
+  mongoose.connect('mongodb://localhost/users_test');
+  // Event handlers for the connection
+  mongoose.connection
+  // Call 'done' once connected in order to run tests
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error)
+    });
+})
+
+// Add hook which is a function that will be executed before each test
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
   });
-
-// testing
+})
