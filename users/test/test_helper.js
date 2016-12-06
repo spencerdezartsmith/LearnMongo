a9df88d926a10d82fd53154b86c4a4dce85a9f03
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-06 12:25:11 -0800

    Create a test helper file to set up the test env

---
+++ users/test/test_helper.js
@@ -0,0 +1,13 @@
+// All code needed to set up our testing environment
+const mongoose = require('mongoose');
+
+// Connect to the database. It will create if the db doesn't exist
+mongoose.connect('mongodb://localhost/users_test');
+// Event handlers for the connection
+mongoose.connection
+  .once('open', () => {
+    console.log('Good to go!')
+  })
+  .on('error', (error) => {
+    console.warn('Warning', error)
+  });
