85ba7db91baa9669c615e215359b56faebe29f3c
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-06 12:24:51 -0800

    Add the code to create the User model and export from module

---
+++ users/src/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String
+});
+
+// Create the model called 'user' with the schema from above
+// We can now refer to the User variable as the 'class' of users
+const User = mongoose.model('user', UserSchema);
+
+// Export the User class
+module.exports = User;
