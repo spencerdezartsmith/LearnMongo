cdbd43fba55ec4e104b9db78c6be3ce0fa602627
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-07 14:18:35 -0800

    Add different syntax for the assert statement

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -14,7 +14,16 @@ describe('Reading users out of the database', () => {
   it('finds all users with the name of joe', (done) => {
     User.find({ name: 'Joe' })
       .then((users) => {
-        console.log(users);
+        assert(users[0].id === joe.id)
+        // assert(users[0]._id.toString() === joe._id.toString())
+        done();
+      });
+  });
+
+  it('find a specific user with a specific id', (done) => {
+    User.findOne({ _id: joe._id })
+      .then((user) => {
+        assert(user.name === 'Joe');
         done();
       });
   });
