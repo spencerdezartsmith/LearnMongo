a8e5129809311fbaa2669182ca58b1515fd09c73
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-09 12:41:21 -0800

    Update test changed to accommodate new schema

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 })
+    joe = new User({ name: 'Joe', likes: 0 })
     joe.save()
       .then(() => done());
   });
@@ -52,11 +52,11 @@ describe('Updating records', () => {
     );
   });
 
-  it('a user can have their postcount incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+  it('a user can have their likes incremented by 10', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 10);
         done();
       });
   });
