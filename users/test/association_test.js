0cabeb71f29599004fbec544701f192fbc3428f0
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-09 12:45:30 -0800

    Add tests for all associations

---
+++ users/test/association_test.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+const assert = require('assert');
+
+describe('Associations', () => {
+  let joe, blogPost, comment;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is great', content: 'Learning is the best' });
+    comment = new Comment({ content: 'You are doing a great job' });
+
+    // User has many blog posts
+    joe.blogPosts.push(blogPost);
+    // Blogpost has many comments
+    blogPost.comments.push(comment);
+    // Comment has one author
+    comment.author = joe;
+    // You can pass Promise.all an array of promises.
+    Promise.all([joe.save(), blogPost.save(), comment.save()])
+      .then(() => done());
+  });
+
+  it('saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title === 'JS is great')
+        done();
+      });
+  });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'author',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is great');
+        assert(user.blogPosts[0].comments[0].content === 'You are doing a great job');
+        assert(user.blogPosts[0].comments[0].author.name === 'Joe');
+
+        done();
+      });
+  });
+});
